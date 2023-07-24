c0622a72f356a7b46c81c785f3a61c6b5ac1dd5d
Ole28 <29503359+Ole28@users.noreply.github.com>
2023-07-24 17:36:57 +0200

    add-robot button implemented: opens/closes add-robot form which emits url onsubmit, gets validated by index.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,29 +5,39 @@ const props = defineProps({
 
 const emits = defineEmits({
     sidebarOpen: () => true,
-    selectConnection: (url: string) => true
+    selectConnection: (url: string) => true,
+    addConnection: (url: string) => true,
 });
 
 const connection = ref(props.urls[0]);
+const expanded = ref(false);
+const url = ref("");
 
 import MaterialSymbolsMenu from '~icons/material-symbols/menu';
 import MaterialSymbolsAddCircle from '~icons/material-symbols/add-circle';
+import IcRoundCheck from '~icons/ic/round-check';
 import { ref } from 'vue';
+
 </script>
 
 <template>
     <header>
-        <button title="Menu" id="sidebar-btn" @click="$emit('sidebarOpen')">
+        <button title="Menu" id="sidebar-btn" @click="$emit('sidebarOpen')" type="button">
             <material-symbols-menu style="font-size: 2em;" />
         </button>
-        <select id="select-robot" v-model="connection" @change="$emit('selectConnection', connection)">
+        <select id="select-robot" v-model="connection" @change="$emit('selectConnection', connection)"
+            title="select connection">
             <option v-for="url in urls" :key="url">
                 {{ url }}
             </option>
         </select>
-        <button id="add-robot-btn">
-            <material-symbols-add-circle style="font-size: 2em;" />
+        <button id="add-robot-btn" @click="expanded = !expanded" title="add new connection" type="button">
+            <material-symbols-add-circle style="font-size: 2em;" :class="expanded ? 'rotated' : {}" />
         </button>
+        <form :class="expanded ? {} : 'hidden'" class="add-robot" @submit.prevent="$emit('addConnection', url)">
+            <input type="text" placeholder="ECTS URL" v-model="url" :disabled="!expanded" />
+            <button type="submit" :disabled="!expanded"><ic-round-check /></button>
+        </form>
     </header>
 </template>
 
@@ -72,6 +82,42 @@ option {
 
 #add-robot-btn {
     background-color: transparent;
+}
 
+#add-robot-btn svg {
+    transition: transform 0.2s;
+}
+
+.rotated {
+    transform: rotate(45deg);
+}
+
+.add-robot.hidden {
+    opacity: 0;
+}
+
+.add-robot {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    transition: opacity 0.2s;
+    opacity: 1;
+    background-color: var(--color-background-mute);
+    border-radius: 8px;
+    align-self: center;
+}
+
+.add-robot input {
+    border: none;
+    border-radius: 8px 0 0 8px;
+    color: var(--text-color);
+    font-size: 1em;
+    align-self: stretch;
+}
+
+.add-robot button {
+    border-radius: 0 8px 8px 0;
+    color: var(--text-color);
+    border: 1px solid white;
 }
 </style>
--- src/components/Plugins/BatteryPlugin/BatteryPlugin.vue
+++ src/components/Plugins/BatteryPlugin/BatteryPlugin.vue
@@ -1,41 +1,44 @@
 <template>
-    <template v-if="usage">
-        <div v-if="usage.power_supply_status == 1" class="item">
+    <template v-if="props.refs.get('/ects/battery/usage')">
+        <div v-if="props.refs.get('/ects/battery/usage').power_supply_status == 1" class="item">
             <material-symbols-power-plug-outline-rounded style="color: green" />
             <span>charging</span>
         </div>
-        <div v-if="usage.percentage" class="item" :style="critical?.data ? { color: 'var(--color-important)' } : {}">
-            <material-symbols-battery20-rounded v-if="usage.percentage > 0.95" />
-            <material-symbols-battery6-bar-rounded v-else-if="usage.percentage > 0.8" />
-            <material-symbols-battery5-bar-rounded v-else-if="usage.percentage > 0.65" />
-            <material-symbols-battery4-bar-rounded v-else-if="usage.percentage > 0.5" />
-            <material-symbols-battery3-bar-rounded v-else-if="usage.percentage > 0.35" />
-            <material-symbols-battery2-bar-rounded v-else-if="usage.percentage > 0.2" />
-            <material-symbols-battery1-bar-rounded v-else-if="usage.percentage > 0.05" />
+        <div v-if="props.refs.get('/ects/battery/usage').percentage" class="item"
+            :style="props.refs.get('/ects/battery/is_critical')?.data ? { color: 'var(--color-important)' } : {}">
+            <material-symbols-battery20-rounded v-if="props.refs.get('/ects/battery/usage').percentage > 0.95" />
+            <material-symbols-battery6-bar-rounded v-else-if="props.refs.get('/ects/battery/usage').percentage > 0.8" />
+            <material-symbols-battery5-bar-rounded v-else-if="props.refs.get('/ects/battery/usage').percentage > 0.65" />
+            <material-symbols-battery4-bar-rounded v-else-if="props.refs.get('/ects/battery/usage').percentage > 0.5" />
+            <material-symbols-battery3-bar-rounded v-else-if="props.refs.get('/ects/battery/usage').percentage > 0.35" />
+            <material-symbols-battery2-bar-rounded v-else-if="props.refs.get('/ects/battery/usage').percentage > 0.2" />
+            <material-symbols-battery1-bar-rounded v-else-if="props.refs.get('/ects/battery/usage').percentage > 0.05" />
             <material-symbols-battery0-bar-rounded v-else />
-            <span>{{ (usage.percentage * 100).toFixed(1) }}% </span>
-            <span v-if="estimated"> ({{ estimated.data }} min) </span>
+            <span>{{ (props.refs.get('/ects/battery/usage').percentage * 100).toFixed(1) }}% </span>
+            <span v-if="props.refs.get('/ects/battery/estimated_time_remaining')">
+                ({{ props.refs.get("/ects/battery/estimated_time_remaining").data }} min)
+            </span>
         </div>
         <div v-else>
             <material-symbols-battery-unknown-rounded />
         </div>
 
-        <div v-if="usage.voltage" class="item">
+        <div v-if="props.refs.get('/ects/battery/usage').voltage" class="item">
             <material-symbols-electric-bolt-rounded />
-            <span>{{ usage.voltage }}V</span>
+            <span>{{ props.refs.get('/ects/battery/usage').voltage }}V</span>
         </div>
 
-        <div v-if="usage.current" class="item">
+        <div v-if="props.refs.get('/ects/battery/usage').current" class="item">
             <material-symbols-power-input-rounded />
-            <span>{{ usage.current }}A</span>
+            <span>{{ props.refs.get('/ects/battery/usage').current }}A</span>
         </div>
 
-        <div v-if="usage.charge" class="item">
+        <div v-if="props.refs.get('/ects/battery/usage').charge" class="item">
             <material-symbols-battery-change-outline-rounded />
-            <span>{{ usage.charge }}Ah</span>
+            <span>{{ props.refs.get('/ects/battery/usage').charge }}Ah</span>
         </div>
 
-        <div v-for="[key, value] in Object.entries(usage).filter(([key, value]) => key != 'percentage' && key != 'voltage' && key != 'current' && key != 'charge' && key != 'power_supply_status')"
+        <div v-for="[key, value] in Object.entries(props.refs.get('/ects/battery/usage')).filter(([key, value]) => key != 'percentage' && key != 'voltage' && key != 'current' && key != 'charge' && key != 'power_supply_status')"
             :key="key" class="details">
             <span>{{ key }}:</span>
             <span>{{ value }}</span>
@@ -44,8 +47,6 @@
 </template>
 
 <script setup lang="ts">
-import { watch } from "vue";
-import { sensor_msgs, std_msgs } from "@/ECTS/Types/Messages";
 
 import MaterialSymbolsBattery20Rounded from '~icons/material-symbols/battery-20-rounded';
 import MaterialSymbolsBatteryUnknownRounded from '~icons/material-symbols/battery-unknown-rounded';
@@ -65,26 +66,8 @@ import MaterialSymbolsPowerPlugOutlineRounded from '~icons/material-symbols/powe
 const props = defineProps({
     refs: { type: Map<string, any>, required: true, default: () => { } }
 });
-const critical = props.refs.get("/ects/battery/is_critical") as std_msgs.Bool;
-watch(() => props.refs.get("/ects/battery/is_critical"), (newVal) => {
-    critical.data = newVal.data;
-});
-const usage = props.refs.get("/ects/battery/usage") as sensor_msgs.BatteryState;
-watch(() => props.refs.get("/ects/battery/usage"), (newVal: sensor_msgs.BatteryState) => {
-    usage.percentage = newVal.percentage;
-    usage.capacity = newVal.capacity;
-    usage.design_capacity = newVal.design_capacity;
-    usage.charge = newVal.charge;
-    usage.current = newVal.current;
-    usage.power_supply_health = newVal.power_supply_health;
-    usage.power_supply_status = newVal.power_supply_status;
-    usage.power_supply_technology = newVal.power_supply_technology;
-    usage.voltage = newVal.voltage;
-});
-const estimated = props.refs.get("/ects/battery/estimated_time_remaining") as std_msgs.Float32;
-watch(() => props.refs.get("/ects/battery/estimated_time_remaining"), (newVal) => {
-    estimated.data = newVal.data;
-});
+
+
 </script>
 
 <style scoped>
