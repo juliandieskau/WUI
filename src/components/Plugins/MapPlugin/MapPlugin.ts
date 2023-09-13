999c69e039a69d4494547148b9ce656d95f403b6
Ole28 <29503359+Ole28@users.noreply.github.com>
2023-09-13 11:42:37 +0200

    fix update suppression

--- src/components/Plugins/MapPlugin/MapPlugin.ts
+++ src/components/Plugins/MapPlugin/MapPlugin.ts
@@ -1,8 +1,5 @@
 import { ECTS } from '@/ECTS/ECTS';
 import { ECTSPlugin } from '@/ECTS/ECTSPlugin';
-import { ects_msgs } from '@/ECTS/Types/Messages';
-import L from 'leaflet';
-import 'leaflet.utm';
 
 export default class MapPlugin extends ECTSPlugin {
   constructor(ects: ECTS) {
@@ -18,29 +15,5 @@ export default class MapPlugin extends ECTSPlugin {
       ])
     });
   }
-  update(topic: string, message: any): void {
-    if (topic === '/ects/waypoints/waypoint_list') {
-      const messageCast = message as ects_msgs.WaypointList;
-      const waypoints = (message as ects_msgs.WaypointList).waypoints;
-      messageCast.waypoints = waypoints.map(waypoint => {
-        const latLng = utmToLatLng(waypoint.pose.x, waypoint.pose.y);
-        waypoint.pose.x = latLng.lat;
-        waypoint.pose.y = latLng.lng;
-        return waypoint;
-      });
-      this.data.set('#waypoint_list', messageCast);
-    }
-    super.update(topic, message);
-  }
-}
-function utmToLatLng(x: number, y: number) {
-  const latLng = L.utm({
-    x: x,
-    y: y,
-    zone: 32,
-    band: 'U',
-    southHemi: false
-  }).latLng();
-  return latLng;
 }
 
